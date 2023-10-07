217804f730d16e1b0ad23bad3fa7f41ae4b06c2e
cmpyre <cmpyre@gmail.com>
2023-10-07 17:59:29 +0200

    Revert "add zone.js import"
    This reverts commit 69c6f7d56c738d2f958f6df1478af1a73617b8af.

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+// import './global-jasmine'
+// import 'jasmine-core/lib/jasmine-core/jasmine-html.js';
+// import 'jasmine-core/lib/jasmine-core/boot.js';
+//
+// import jasmineRequire from 'jasmine-core/lib/jasmine-core.js';
+
+import 'zone.js';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+// window['jasmineRequire'] = jasmineRequire;
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
